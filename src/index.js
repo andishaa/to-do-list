cd938b670bdcdf2bdb8962033d0f14fafd0d429b
andisha <andon.stoichkov@yahoo.com>
2022-09-27 18:37:10 +0300

    Add different priority test toDos for better visualisation CSS

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import { initDomLoad } from './DOM';
 
 let dedoviyaProject = projects.CreateNewProject('Dedoviya');
 let inbox = projects.CreateNewProject('Inbox');
-let testToDo = toDos.ToDoFactory('test', 'dedoviyaaaaaaaa', '2022-20-03', 'low',);
-let testToDo2 = toDos.ToDoFactory('test234', 'asdfgh', '203322-20-03', 'low',);
+let testToDo = toDos.ToDoFactory('testLow', 'dedoviyaaaaaaaa', '2022-10-03', 'low',);
+let testToDo2 = toDos.ToDoFactory('testMedium', 'asdfgh', '2022-08-23', 'medium',);
+let testToDo3 = toDos.ToDoFactory('testHigh', 'test description', '2022-09-03', 'high')
 dedoviyaProject.addToDo(testToDo);
 inbox.addToDo(testToDo2);
-inbox.addToDo(testToDo2);
-inbox.addToDo(testToDo2);
-
-testToDo.editTitle('changedTitle asdfg');
+inbox.addToDo(testToDo3);
+testToDo.editTitle('changedTitlLow asdfg');
 
 
 function initialPageLoad(){
