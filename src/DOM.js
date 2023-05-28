36b4d00f63dabc3a96eb6e27448f0d62eca2109e
Andon Stoichkov <andon.stoichkov@yahoo.com>
2023-05-28 14:57:06 +0300

    Refactor DOM module
    Added a function that creates DOM elements and used it instead of
    manually creating each element

--- src/DOM.js
+++ src/DOM.js
@@ -1,90 +1,89 @@
+/**
+ * Creates a new DOM element with optional attributes and text content.
+ * @param {string} tagName - The tag name of the DOM element to create.
+ * @param {object} attributes - An object containing attributes and their values to be set on the created element.
+ * @param {string} text - The text content to set for the element.
+ * @returns {HTMLElement} The created DOM element.
+ */
+function createDOMElement(tagName, attributes, text) {
+    // Create a new DOM element using the specified tag name
+    let element = document.createElement(tagName);
+
+    // Set attributes on the element, if provided
+    if (attributes) {
+        for (let attr in attributes) {
+            if (attributes.hasOwnProperty(attr)) {
+                element.setAttribute(attr, attributes[attr]);
+            }
+        }
+    }
+
+    // Set text content on the element, if provided
+    if (text) {
+        let textNode = document.createTextNode(text);
+        element.appendChild(textNode);
+    }
+
+    // Return the created DOM element
+    return element;
+}
+
 const header = () => {
-    const header = document.createElement('header');
-    header.classList.add('header');
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Organize your life';
-    header.appendChild(h1);
+    const headerElement = createDOMElement('header', { class: 'header' })
+    const h1 = createDOMElement('h1', {}, 'Organize your life')
+    headerElement.appendChild(h1);
 
     //add a button for mobile screens to toggle the navigation bar
-    const toggleNavBtn = document.createElement('button');
-    toggleNavBtn.classList.add('toggle-nav-btn');
-    toggleNavBtn.textContent = "☰";
-    header.appendChild(toggleNavBtn);
+    const toggleNavBtn = createDOMElement('button', { class: 'toggle-nav-btn' }, '☰');
+    headerElement.appendChild(toggleNavBtn);
 
-    return header;
+    return headerElement;
 }
 
 const nav = () => {
-    const nav = document.createElement('nav');
-    nav.classList.add('nav');
+    const nav = createDOMElement('nav', { class: 'nav' });
 
-    const mainSection = document.createElement('div');
-    mainSection.classList.add('main-nav')
+    const mainSection = createDOMElement('div', { class: 'main-nav' });
 
-    const mainUl = document.createElement('ul');
-    const inboxLi = document.createElement('li');
-    inboxLi.classList.add('nav-active', 'project-name');
-    inboxLi.textContent = 'Inbox';
-    mainUl.append(inboxLi);
+    const mainUl = createDOMElement('ul');
+    const inboxLi = createDOMElement('li', { class: 'project-name nav-active' }, 'Inbox');
+    mainUl.appendChild(inboxLi);
 
-    const todayLi = document.createElement('li');
-    todayLi.classList.add('project-name');
-    todayLi.textContent = 'Today';
-    mainUl.append(todayLi);
+    const todayLi = createDOMElement('li', { class: 'project-name' }, 'Today');
+    mainUl.appendChild(todayLi);
 
-    const thisWeekLi = document.createElement('li');
-    thisWeekLi.classList.add('project-name');
-    thisWeekLi.textContent = 'This Week';
-    mainUl.append(thisWeekLi);
+    const thisWeekLi = createDOMElement('li', { class: 'project-name' }, 'This Week');
+    mainUl.appendChild(thisWeekLi);
 
-    const projectsSection = document.createElement('div');
-    projectsSection.classList.add('projects-nav')
-    const projectsSectionTitle = document.createElement('h1');
-    projectsSectionTitle.textContent = 'Projects';
-    projectsSection.append(projectsSectionTitle);
+    const projectsSection = createDOMElement('div', { class: 'projects-nav' });
+    const projectsSectionTitle = createDOMElement('h1', {}, 'Projects');
+    projectsSection.appendChild(projectsSectionTitle);
 
-    const projectsDiv = document.createElement('div');
-    projectsDiv.classList.add('projects-list');
-    projectsSection.append(projectsDiv);
+    const projectsDiv = createDOMElement('div', { class: 'projects-list' });
+    projectsSection.appendChild(projectsDiv);
 
-    const addProjectBtn = document.createElement('button');
-    addProjectBtn.id = 'add-project-btn';
-    addProjectBtn.textContent = 'Add project';
-    projectsSection.append(addProjectBtn);
+    const addProjectBtn = createDOMElement('button', { id: 'add-project-btn' }, 'Add Project');
+    projectsSection.appendChild(addProjectBtn);
 
-    mainSection.append(mainUl);
-    nav.append(mainSection, projectsSection);
+    mainSection.appendChild(mainUl);
+    nav.appendChild(mainSection);
+    nav.appendChild(projectsSection);
 
     return nav;
 }
 
 const projectForm = () => {
-    const projectForm = document.createElement('form');
-    projectForm.classList.add('project-form');
-
-    const input = document.createElement('input');
-    input.classList.add('form-input');
-    input.type = 'text';
-    input.placeholder = 'Project name';
+    const projectForm = createDOMElement('form', { class: 'project-form' });
+    const input = createDOMElement('input', { class: 'form-input', type: 'text', placeholder: 'Project name' });
     projectForm.appendChild(input);
 
-    const projectErrorSpan = document.createElement('span')
-    projectErrorSpan.id = 'projectError';
-    projectErrorSpan.classList.add('error');
+    const projectErrorSpan = createDOMElement('span', { id: 'projectError', class: 'error' })
     projectForm.appendChild(projectErrorSpan);
 
-    const addBtn = document.createElement('button');
-    addBtn.textContent = 'Add';
-    addBtn.id = 'form-add-btn';
-    addBtn.classList.add('green-btn');
-    addBtn.type = 'button';
+    const addBtn = createDOMElement('button', { type: 'button', id: 'form-add-btn', class: 'green-btn' }, 'Add');
     projectForm.appendChild(addBtn);
 
-    const cancelBtn = document.createElement('button');
-    cancelBtn.id = 'form-cancel-btn';
-    cancelBtn.classList.add('red-btn');
-    cancelBtn.textContent = 'Cancel';
-    cancelBtn.type = 'button';
+    const cancelBtn = createDOMElement('button', { type: 'button', id: 'form-cancel-btn', class: 'red-btn' }, 'Cancel');
     projectForm.appendChild(cancelBtn);
 
     return projectForm;
@@ -92,89 +91,49 @@ const projectForm = () => {
 
 function createProjectListItemDiv(projectObj) {
     const projectsListDiv = document.querySelector('.projects-list');
-
-    const projectListItemDiv = document.createElement('div');
-    projectListItemDiv.classList.add('projects-list-item');
-
-    const nameContainerDiv = document.createElement('div');
-    nameContainerDiv.classList.add('project-name');
-    nameContainerDiv.textContent = projectObj.getName();
-
-    const deleteBtnContainerDiv = document.createElement('div');
-    deleteBtnContainerDiv.classList.add('del-project-btn');
-    deleteBtnContainerDiv.textContent = 'X';
+    const projectListItemDiv = createDOMElement('div', { class: 'projects-list-item' });
+    const nameContainerDiv = createDOMElement('div', { class: 'project-name' }, projectObj.getName());
+    const deleteBtnContainerDiv = createDOMElement('div', { class: 'del-project-btn' }, 'X');
 
     projectListItemDiv.append(nameContainerDiv, deleteBtnContainerDiv);
     projectsListDiv.append(projectListItemDiv);
 }
 
 const main = () => {
-    const mainEl = document.createElement('main');
-    mainEl.classList.add('main');
-
-    const toDosDiv = document.createElement('div');
-    toDosDiv.classList.add('todos-container');
-    const showToDoFormBtn = toDoFormBtn();
+    const mainEl = createDOMElement('main', { class: 'main' });
+    const toDosDiv = createDOMElement('div', { class: 'todos-container' });
+    const showToDoFormBtn = createDOMElement('button', { id: 'todo-form-btn' }, 'Add new To-Do');
     const addToDoForm = addNewToDoForm();
+
     mainEl.append(showToDoFormBtn, addToDoForm, toDosDiv);
 
     return mainEl;
 }
 
-const toDoFormBtn = () => {
-    const addToDoBtn = document.createElement('button');
-    addToDoBtn.id = 'todo-form-btn';
-    addToDoBtn.textContent = 'Add new To-Do';
-
-    return addToDoBtn;
-}
-
 const addNewToDoForm = () => {
-    const toDoForm = document.createElement('form');
-    toDoForm.classList.add('todo-form', 'hidden'); //make it hidden by default
-
-    const titleInput = document.createElement('input');
-    titleInput.classList.add('todo-title');
-    titleInput.placeholder = 'To Do Title';
-    titleInput.type = 'text';
-    titleInput.required = true;
+    const toDoForm = createDOMElement('form', { class: 'todo-form hidden' });
+    const titleInput = createDOMElement('input', { class: 'todo-title', type: 'text', required: '', placeholder: 'Title' });
     toDoForm.appendChild(titleInput);
 
-    const titileErrorSpan = document.createElement('span');
-    titileErrorSpan.id = 'titleError';
-    titileErrorSpan.classList.add('error');
+    const titileErrorSpan = createDOMElement('span', { id: 'titleError', class: 'error' });
     toDoForm.appendChild(titileErrorSpan);
 
-    const descriptionInput = document.createElement('textarea');
-    descriptionInput.classList.add('todo-description');
-    descriptionInput.placeholder = 'Description (not required)';
+    const descriptionInput = createDOMElement('textarea', { class: 'todo-description', placeholder: 'Description (not required)' });
     toDoForm.appendChild(descriptionInput);
 
-    const dueDateInput = document.createElement('input');
-    dueDateInput.classList.add('todo-duedate');
-    dueDateInput.type = 'date';
+    const dueDateInput = createDOMElement('input', { type: 'date', class: 'todo-duedate', required: '' });
     dueDateInput.valueAsDate = new Date(); // by default set the date to Today's day
-    dueDateInput.required = true;
     toDoForm.appendChild(dueDateInput);
 
     const prioritySelect = toDoPrioritySelectElement();
     toDoForm.appendChild(prioritySelect);
 
-    const buttonsSpan = document.createElement('span');
-    buttonsSpan.classList.add('add-cancel-todo-btns');
+    const buttonsSpan = createDOMElement('span', { class: 'add-cancel-todo-btns' });
 
-    const addToDoBtn = document.createElement('button');
-    addToDoBtn.textContent = 'Add';
-    addToDoBtn.id = 'todo-add-btn';
-    addToDoBtn.classList.add('green-btn');
-    addToDoBtn.type = 'button';
+    const addToDoBtn = createDOMElement('button', { type: 'button', id: 'todo-add-btn', class: 'green-btn' }, 'Add');
     buttonsSpan.appendChild(addToDoBtn);
 
-    const cancelToDoBtn = document.createElement('button');
-    cancelToDoBtn.textContent = 'Cancel';
-    cancelToDoBtn.id = 'todo-cancel-btn';
-    cancelToDoBtn.classList.add('red-btn');
-    cancelToDoBtn.type = 'button';
+    const cancelToDoBtn = createDOMElement('button', { type: 'button', id: 'todo-cancel-btn', class: 'red-btn' }, 'Cancel');
     buttonsSpan.appendChild(cancelToDoBtn);
     toDoForm.appendChild(buttonsSpan);
 
@@ -182,86 +141,51 @@ const addNewToDoForm = () => {
 }
 
 function toDoPrioritySelectElement() {
-    const prioritySelectElement = document.createElement('select');
-    prioritySelectElement.id = 'priority-input';
-    const optionDefault = document.createElement('option');
-    optionDefault.textContent = '--Priority';
-    optionDefault.value = 'none';
-    prioritySelectElement.append(optionDefault);
+    const prioritySelectElement = createDOMElement('select', { id: 'priority-input' });
 
-    const optionLow = document.createElement('option');
-    optionLow.textContent = 'Low Priority';
-    optionLow.value = 'low';
-    prioritySelectElement.append(optionLow);
+    const optionDefault = createDOMElement('option', { value: 'none' }, '--Priority');
+    prioritySelectElement.appendChild(optionDefault);
 
-    const optionMedium = document.createElement('option');
-    optionMedium.textContent = 'Medium Priority';
-    optionMedium.value = 'medium';
-    prioritySelectElement.append(optionMedium);
+    const optionLow = createDOMElement('option', { value: 'low' }, 'Low Priority');
+    prioritySelectElement.appendChild(optionLow);
 
-    const optionHigh = document.createElement('option');
-    optionHigh.textContent = 'High Priority';
-    optionHigh.value = 'high';
-    prioritySelectElement.append(optionHigh);
+    const optionMedium = createDOMElement('option', { value: 'medium' }, 'Medium Priority');
+    prioritySelectElement.appendChild(optionMedium);
+
+    const optionHigh = createDOMElement('option', { value: 'high' }, 'High Priority');
+    prioritySelectElement.appendChild(optionHigh);
 
     return prioritySelectElement;
 }
 
 function createToDoCardDiv(toDoObj) {
     const toDosDivContainer = document.querySelector('.todos-container');
-
-    const toDoCardDiv = document.createElement('div');
-    toDoCardDiv.classList.add('todo-card', `${toDoObj.getPriority()}`); // add the priority status of the ToDo as a class to be able to change it's styles
-    toDoCardDiv.id = toDoObj.getID();
-
-    const toDoMain = document.createElement('div');
-    toDoMain.classList.add('todo-main');
-
-    const cardTitleDiv = document.createElement('div');
-    cardTitleDiv.classList.add('card-title');
-    cardTitleDiv.textContent = toDoObj.getTitle();
+    const toDoCardDiv = createDOMElement('div', { id: toDoObj.getID(), class: `todo-card ${toDoObj.getPriority()}` });
+    const toDoMain = createDOMElement('div', { class: 'todo-main' });
+    const cardTitleDiv = createDOMElement('div', { class: 'card-title' }, toDoObj.getTitle());
     toDoMain.append(cardTitleDiv);
-
-    const cardDueDateDiv = document.createElement('div');
-    cardDueDateDiv.classList.add('card-duedate');
-    cardDueDateDiv.textContent = toDoObj.getDueDate();
+    const cardDueDateDiv = createDOMElement('div', { class: 'card-duedate' }, toDoObj.getDueDate());
     toDoMain.append(cardDueDateDiv);
-
-    const cardBtns = document.createElement('div');
-    cardBtns.classList.add('card-btns');
-    const showMore = document.createElement('button');
-    showMore.classList.add('btn', 'show-more-btn');
-    showMore.textContent = '>>';
+    const cardBtns = createDOMElement('div', { class: 'card-btns' });
+    const showMore = createDOMElement('button', { class: 'btn show-more-btn' }, '>>');
     cardBtns.append(showMore);
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.classList.add('btn', 'delete-btn');
-    deleteBtn.textContent = 'Delete';
+    const deleteBtn = createDOMElement('button', { class: 'btn delete-btn' }, 'Delete');
     cardBtns.append(deleteBtn);
 
     toDoMain.append(cardBtns);
     toDoCardDiv.append(toDoMain);
 
-    const toDoDetails = document.createElement('div');
-    toDoDetails.classList.add('todo-details');
-    toDoDetails.setAttribute('style', 'display: none'); // by default hide the details div of the todo on rendering
+    const toDoDetails = createDOMElement('div', { class: 'todo-details', style: 'display: none' });
 
-    const description = document.createElement('div');
-    description.textContent = 'Description: ';
-    description.classList.add('card-description');
-    const descriptionInfo = document.createElement('span');
-    descriptionInfo.classList.add('description-info');
-    descriptionInfo.textContent = toDoObj.getDescription();
-    //if the toDo is with empty description, add by default this content:
+    const description = createDOMElement('div', { class: 'card-description' }, 'Description');
+    const descriptionInfo = createDOMElement('span', { class: 'description-info' }, toDoObj.getDescription());
     if (toDoObj.getDescription() === '') {
         descriptionInfo.textContent = 'Empty description';
     }
     description.append(descriptionInfo);
     toDoDetails.append(description);
 
-    const toDoPriority = document.createElement('span');
-    toDoPriority.classList.add('todo-priority');
-    toDoPriority.textContent = `Priority: ${toDoObj.getPriority()}`;
+    const toDoPriority = createDOMElement('span', { class: 'todo-priority' }, `Priority: ${toDoObj.getPriority()}`);
     toDoDetails.append(toDoPriority);
 
     toDoCardDiv.append(toDoDetails);
